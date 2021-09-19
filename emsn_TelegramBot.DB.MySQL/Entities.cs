51a00636e86fd72d49eeacf9749e262a2b7d4191
Sillyname <sillyname_dev@inbox.ru>
2021-09-19 14:39:25 +0300

    Наполнение сущностей БД
    (Пользователь, Роль)

--- emsn_TelegramBot.DB.MySQL/Entities.cs
+++ emsn_TelegramBot.DB.MySQL/Entities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,18 @@ namespace emsn_TelegramBot.DB.MySQL
 {
     class Entities
     {
+        [Table("user")]
+        public class User //Пользователь бота
+        {
+            public int id { get; set; } //Идентификатор пользователя (в соответствии с Telegram API)
+            public string authCode { get; set; } //Код авторизации от бота
+
+        }
+
+        [Table("role")]
+        public class Role //Роль пользователя в соответствии с ролевой моделью
+        {
+            public string roleName { get; set; } //наименование роли пользователя в системе
+        }
     }
 }
